8a7377bfba150489a59ae39a2d8dc3e26ac57470
Fred Tempez <ftempez@gmail.com>
2020-04-07 17:36:24 +0200

    [9.2.28] Compatibilité user edit module système avec module auto inscription

--- core/core.php
+++ core/core.php
@@ -33,7 +33,7 @@ class common {
 	const TEMP_DIR = 'site/tmp/';
 
 	// Numéro de version 
-	const ZWII_VERSION = '9.2.27';
+	const ZWII_VERSION = '9.2.28';
 	const ZWII_UPDATE_CHANNEL = "v9";
 
 	public static $actions = [];
