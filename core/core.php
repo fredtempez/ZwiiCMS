1b2fd01535f8e9ffe2562f9b98a58442071450ef
Fred Tempez <ftempez@gmail.com>
2020-04-19 19:24:23 +0200

    Effacer le cache au changement de données du thème.

--- core/core.php
+++ core/core.php
@@ -1406,6 +1406,12 @@ class core extends common {
 			}
 			// Enregistre la personnalisation
 			file_put_contents(self::DATA_DIR.'theme.css', $css);
+			// Effacer le cache pour afin de tenir compte de la couleur de fond TinyMCE
+			header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+			header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+			header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+			header("Cache-Control: post-check=0, pre-check=0", false);
+			header("Pragma: no-cache");
 		}
 	}
 	/**
