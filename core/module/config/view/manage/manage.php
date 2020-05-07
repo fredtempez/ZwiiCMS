4bab683c2bc7378a5230f8e4216c9deeb13236bc
Fred Tempez <ftempez@gmail.com>
2020-05-07 08:45:46 +0200

    087 config backup de la v10 WIP

--- core/module/config/view/manage/manage.php
+++ core/module/config/view/manage/manage.php
@@ -13,22 +13,26 @@
 	<div class="col12">
 		<div class="block">
 			<h4>Restauration ou transfert d'un site</h4>
-			<div class="row">				
-				<?php echo template::file('configManageImportFile', [
-					'label' => 'Sélectionnez une archive au format ZIP',
-					'type' => 2,
-					'help' => 'L\'archive a été déposée dans le gestionaire de fichiers. Les archives inférieures à la version 9 ne sont pas acceptées.'
-				]); ?>
-			</div>
 			<div class="row">
-				<?php echo template::checkbox('configManageImportUser', true, 'Préserver les comptes des utilisateurs déjà installés', [
-					'checked' => true
-				]); ?>			
-			</div>	
-			<div class="col4 offset4">
-				<?php echo template::submit('configManageSubmit',[
-					'value' => 'Restaurer'
-				]); ?>
+				<div class="col6">
+					<div class="row">
+						<?php echo template::file('configManageImportFile', [
+							'label' => 'Sélectionnez une archive au format ZIP',
+							'type' => 2,
+							'help' => 'L\'archive a été déposée dans le gestionaire de fichiers. Les archives inférieures à la version 9 ne sont pas acceptées.'
+						]); ?>
+					</div>
+					<div class="row">
+						<?php echo template::checkbox('configManageImportUser', true, 'Préserver les comptes des utilisateurs déjà installés', [
+							'checked' => true
+						]); ?>		
+					</div>		
+				</div>
+				<div class="col3 offset2">
+					<?php echo template::submit('configManageSubmit',[
+						'value' => 'Restaurer'
+					]); ?>
+				</div>
 			</div>		
 		</div>				
 	</div>		
@@ -45,13 +49,13 @@
 						$buttonClass = 'disabled'; 
 					} elseif ($this->getData(['core', 'baseUrl']) === '') {
 						$baseUrlValue = '/';
-						$buttonClass = (helper::baseUrl(false,false) !== $this->getData(['core', 'baseUrl']) ) ? '' : 'disabled';
+						$buttonClass = helper::baseUrl(false,false) !== $this->getData(['core', 'baseUrl']) ? '' : 'disabled';
 					} else {
-						$baseUrlValue = $this->getData(['core', 'baseUrl']);
-						$buttonClass = (helper::baseUrl(false,false) !== $this->getData(['core', 'baseUrl']) ) ? '' : 'disabled';
+						$baseUrlValue = str_replace('?','',$this->getData(['core', 'baseUrl']));
+						$buttonClass = helper::baseUrl(false,false) !== $baseUrlValue ? '' : 'disabled';
 					}
 					echo template::text('configManageBaseURLToConvert', [
-						'label' => 'Dossier d\'installation de l\'archive' ,
+						'label' => 'Dossier de l\'archive' ,
 						'value' => $baseUrlValue,
 						'readonly' => true,
 						'help'  => 'Lors de la restauration d\'un backup d\'une version 9.2.10 ou supérieure, l\'URL de base est stockée dans la configuration sinon cette donnée est vide.'
