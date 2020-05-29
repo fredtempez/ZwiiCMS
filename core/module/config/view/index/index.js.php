d1272cb3c65a9ce794990dcb8680cf816c6e4785
Fred Tempez <ftempez@gmail.com>
2020-05-29 17:53:30 +0200

    10.2.dev14 logique des options de sécurité de connexion

--- core/module/config/view/index/index.js.php
+++ core/module/config/view/index/index.js.php
@@ -10,41 +10,41 @@
  * @link http://zwiicms.com/
  */
 
-$( document).ready(function() {	 
+$( document).ready(function() {
     /**
      * Afficher et masquer options SMTP
      */
     if ($("input[name=configSmtpEnable]").is(':checked')) {
         $("#configSmtpParam").addClass("disabled");
-        $("#configSmtpParam").slideDown();        
+        $("#configSmtpParam").slideDown();
     } else {
         $("#configSmtpParam").removeClass("disabled");
-        $("#configSmtpParam").slideUp();        
+        $("#configSmtpParam").slideUp();
     }
 
     /**
      * Afficher et masquer options Auth
      */
 
-    if ($("select[name=configSmtpAuth]").val() == true) {     
+    if ($("select[name=configSmtpAuth]").val() == true) {
         $("#configSmtpAuthParam").addClass("disabled");
-        $("#configSmtpAuthParam").slideDown();               
-    } else {  
+        $("#configSmtpAuthParam").slideDown();
+    } else {
         $("#configSmtpAuthParam").removeClass("disabled");
-        $("#configSmtpAuthParam").slideUp();            
+        $("#configSmtpAuthParam").slideUp();
     }
 });
 
 /**
  * Afficher et masquer options SMTP
  */
-$("input[name=configSmtpEnable]").on("change", function() {    
+$("input[name=configSmtpEnable]").on("change", function() {
     if ($("input[name=configSmtpEnable]").is(':checked')) {
         $("#configSmtpParam").addClass("disabled");
-        $("#configSmtpParam").slideDown();        
+        $("#configSmtpParam").slideDown();
     } else {
         $("#configSmtpParam").removeClass("disabled");
-        $("#configSmtpParam").slideUp();        
+        $("#configSmtpParam").slideUp();
     }
 });
 
@@ -52,12 +52,27 @@ $("input[name=configSmtpEnable]").on("change", function() {
  * Afficher et masquer options Auth
  */
 
-$("select[name=configSmtpAuth]").on("change", function() {    
-    if ($("select[name=configSmtpAuth]").val() == true) {     
+$("select[name=configSmtpAuth]").on("change", function() {
+    if ($("select[name=configSmtpAuth]").val() == true) {
         $("#configSmtpAuthParam").addClass("disabled");
-        $("#configSmtpAuthParam").slideDown();               
-    } else {  
+        $("#configSmtpAuthParam").slideDown();
+    } else {
         $("#configSmtpAuthParam").removeClass("disabled");
-        $("#configSmtpAuthParam").slideUp();            
+        $("#configSmtpAuthParam").slideUp();
+    }
+});
+
+/**
+ * Options de blocage de connexions choix aucun
+ */
+$("select[name=configConnectAttempt]").on("change", function() {
+    if ($("select[name=configConnectAttempt]").val() === "999") {
+        $("select[name=configConnectTimeout]").val(0);
+    }
+});
+
+$("select[name=configConnectTimeout]").on("change", function() {
+    if ($("select[name=configConnectTimeout]").val() === "0") {
+        $("select[name=configConnectAttempt]").val(999);
     }
 });
