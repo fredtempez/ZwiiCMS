103771780968e0816eff0e0841849afc6dfe16b9
Fred Tempez <ftempez@gmail.com>
2020-05-30 18:26:05 +0200

    Suppression plugin fullpage tinymce

--- core/vendor/tinymce/init.js
+++ core/vendor/tinymce/init.js
@@ -161,16 +161,6 @@ tinymce.init({
 			description: "Bloc de texte avec effet accordéon."
 		},
 		{
-			title: "FullPage sections",
-			url: baseUrl + "core/vendor/tinymce/templates/fullPageSections.html",
-			description: "Page organisée en sections navigable avec le module redirection."
-		},		
-		{
-			title: "FullPage slides",
-			url: baseUrl + "core/vendor/tinymce/templates/fullPageSlides.html",
-			description: "Page organisée en slides navigable avec le module redirection."
-		},		
-		{
 			title: "Grille symétrique : 6 - 6",
 			url: baseUrl + "core/vendor/tinymce/templates/col6.html",
 			description: "Grille adaptative sur 12 colonnes, sur mobile elles passent les unes en dessous des autres."
