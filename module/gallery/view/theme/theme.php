4975fa74c45c5aa9d7ddfd7a936cb25abadaf568
Fred Tempez <ftempez@gmail.com>
2020-06-16 12:43:54 +0200

    Gallery 2.26 largeur proportionnelle

--- module/gallery/view/theme/theme.php
+++ module/gallery/view/theme/theme.php
@@ -10,7 +10,7 @@
 		</div>
 		<div class="col2 offset8">
 			<?php echo template::submit('galleryThemeBack'); ?>
-		</div>		
+		</div>
 	</div>
     <div class="row">
         <div class="col12">
@@ -18,17 +18,17 @@
                 <h4>Configuration des vignettes</h4>
                 <div class="row">
                     <div class="col3">
-                        <?php echo template::select('galleryThemeThumbWidth', $module::$galleryThemeSize, [
+                        <?php echo template::select('galleryThemeThumbWidth', $module::$galleryThemeSizeWidth, [
                             'label' => 'Largeur',
                             'selected' => $this->getData(['theme', $this->getUrl(0),'thumbWidth'])
                         ]); ?>
                     </div>
                     <div class="col3">
-                        <?php echo template::select('galleryThemeThumbHeight', $module::$galleryThemeSize, [
+                        <?php echo template::select('galleryThemeThumbHeight', $module::$galleryThemeSizeHeight, [
                             'label' => 'Hauteur',
                             'selected' => $this->getData(['theme', $this->getUrl(0),'thumbHeight'])
                         ]); ?>
-                    </div>                  
+                    </div>
                     <div class="col4">
                         <?php echo template::select('galleryThemeThumbAlign', $module::$galleryThemeFlexAlign, [
 							'label' => 'Alignement',
@@ -48,15 +48,15 @@
                             'label' => 'Bordure',
                             'selected' => $this->getData(['theme', $this->getUrl(0),'thumbBorder'])
                         ]); ?>
-                    </div>                   
+                    </div>
                     <div class="col4">
                         <?php echo template::text('galleryThemeThumbBorderColor', [
                             'class' => 'colorPicker',
-                            'help' => 'Le curseur horizontal règle le niveau de transparence.',							
+                            'help' => 'Le curseur horizontal règle le niveau de transparence.',
                             'label' => 'Couleur de la bordure',
                             'value' => $this->getData(['theme', $this->getUrl(0),'thumbBorderColor'])
                         ]); ?>
-                    </div> 
+                    </div>
                     <div class="col4">
                         <?php echo template::select('galleryThemeThumbRadius', $module::$galleryThemeRadius, [
                             'label' => 'Arrondi des angles',
@@ -64,7 +64,7 @@
                         ]); ?>
                     </div>
                 </div>
-                <div class="row">   
+                <div class="row">
                     <div class="col4">
                         <?php echo template::select('galleryThemeThumbShadows', $module::$galleryThemeShadows, [
                             'label' => 'Ombre',
@@ -74,20 +74,20 @@
                     <div class="col4">
                         <?php echo template::text('galleryThemeThumbShadowsColor', [
                             'class' => 'colorPicker',
-                            'help' => 'Le curseur horizontal règle le niveau de transparence.',							
+                            'help' => 'Le curseur horizontal règle le niveau de transparence.',
                             'label' => 'Couleur de l\'ombre',
                             'value' => $this->getData(['theme', $this->getUrl(0),'thumbShadowsColor'])
                         ]); ?>
-                    </div>                                        
+                    </div>
                     <div class="col4">
                         <?php echo template::select('galleryThemeThumbOpacity', $module::$galleryThemeOpacity, [
                             'label' => 'Opacité au survol',
                             'selected' => $this->getData(['theme', $this->getUrl(0),'thumbOpacity'])
                         ]); ?>
-                    </div>                                  
+                    </div>
                 </div>
-            </div>           
-        </div>        
+            </div>
+        </div>
     </div>
     <div class="row">
         <div class="col12">
@@ -97,19 +97,19 @@
                 <div class="col3">
                     <?php echo template::text('galleryThemeLegendTextColor', [
                         'class' => 'colorPicker',
-                        'help' => 'Le curseur horizontal règle le niveau de transparence.',							
+                        'help' => 'Le curseur horizontal règle le niveau de transparence.',
                         'label' => 'Texte',
                         'value' => $this->getData(['theme', $this->getUrl(0),'legendTextColor'])
-                    ]); ?>	
+                    ]); ?>
                 </div>
                 <div class="col3">
                     <?php echo template::text('galleryThemeLegendBgColor', [
                         'class' => 'colorPicker',
-                        'help' => 'Le curseur horizontal règle le niveau de transparence.',							
+                        'help' => 'Le curseur horizontal règle le niveau de transparence.',
                         'label' => 'Fond',
                         'value' => $this->getData(['theme', $this->getUrl(0),'legendBgColor'])
-                    ]); ?>	
-                </div>                
+                    ]); ?>
+                </div>
                 <div class="col3">
                     <?php echo template::select('galleryThemeLegendHeight', $module::$galleryThemeLegendHeight, [
                         'label' => 'Hauteur',
@@ -125,11 +125,11 @@
             </div>
         </div>
     </div>
-<?php echo template::formClose(); ?>	
+<?php echo template::formClose(); ?>
 <div class="row">
 	<div class="col12">
     	<div class="moduleVersion">Version n°
 		<?php echo $module::GALLERY_VERSION; ?>
         </div>
 	</div>
-</div>    
+</div>
