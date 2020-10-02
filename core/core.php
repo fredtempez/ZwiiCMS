06d71f44f6835ed79fcbe78d4b5dc879c1af8c25
Fred Tempez <ftempez@gmail.com>
2020-10-02 16:49:21 +0200

    NUméro de version systeme

--- core/core.php
+++ core/core.php
@@ -39,7 +39,7 @@ class common {
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '10.3.01';
+	const ZWII_VERSION = '10.3.02';
 	const ZWII_UPDATE_CHANNEL = "v10";
 
 	public static $actions = [];
