cbe9b1a1e26fdc3730e8bc21d9c318f054f7780c
fredtempez <ftempez@gmail.com>
2021-01-07 14:29:32 +0100

    branch name

--- core/core.php
+++ core/core.php
@@ -40,7 +40,7 @@ class common {
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '10.3.13.multi';
+	const ZWII_VERSION = '10.3.13.multual';
 	const ZWII_UPDATE_CHANNEL = "v10";
 
 	public static $actions = [];
