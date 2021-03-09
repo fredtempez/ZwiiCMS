a0a259f92f75d8782e6c403702cc436ec1d89f08
Fred Tempez <frederic.tempez@outlook.com>
2021-03-09 16:15:54 +0100

    Détail brut d'un item du catalogue

--- core/module/addon/view/item/item.php
+++ core/module/addon/view/item/item.php
@@ -9,27 +9,27 @@
 	</div>
 </div>
 <div class="row">
-    <div class="col12">
-        <div class="block">
-        <h4> <?php echo $module::$storeItem['title'];?>
-            ; version  <?php echo $module::$storeItem['fileVersion'];?>
-             du <?php echo $module::$storeItem['fileDate'];?></h4>
-            <div class="row">
-                <div class="col9">
-                    <?php echo $module::$storeItem['content'];?>
-                </div>
-                <div class="col3">
-                    <div class="row">
-                        <div class="col12">
-                            <img src="https://www.zwiicms.fr/site/file/source/<?php echo $module::$storeItem['picture'];?>"/>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col12 textAlignCenter">
-
-                        </div>
-                    </div>
-                </div>
+    <div class="col9">
+        <div class="row">
+            <div class="col9">
+                <?php echo $module::$storeItem['content'];?>
+            </div>
+        </div>
+    </div>
+    <div class="col3">
+        <div class="row">
+            <div class="col12">
+                <img src="https://www.zwiicms.fr/site/file/source/<?php echo $module::$storeItem['picture'];?>"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col12">
+            Version <?php echo $module::$storeItem['fileVersion'];?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col12"> 
+             Publié le <?php echo $module::$storeItem['fileDate'];?>
             </div>
         </div>
     </div>
