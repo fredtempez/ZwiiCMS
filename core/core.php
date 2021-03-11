1695d7d2424a0eacf0536964124de38942a16877
Fred Tempez <frederic.tempez@outlook.com>
2021-03-11 10:40:00 +0100

    core version

--- core/core.php
+++ core/core.php
@@ -44,7 +44,7 @@ class common {
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '10.4.05';
+	const ZWII_VERSION = '10.4.06';
 	const ZWII_UPDATE_CHANNEL = "v10";
 
 	public static $actions = [];
