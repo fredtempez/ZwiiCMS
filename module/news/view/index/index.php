00e60059839c484ee8778a1336197f804fb0e4ba
Fred Tempez <frederic.tempez@outlook.com>
2021-03-18 14:30:34 +0100

    news bouton léger d'édition

--- module/news/view/index/index.php
+++ module/news/view/index/index.php
@@ -2,21 +2,36 @@
 	<div class="row">
 		<div class="col12">
 			<?php foreach($module::$news as $newsId => $news): ?>
-				<h1 class="newsTitle" id="<?php echo $newsId;?>">
-					<?php echo $news['title']; ?>
-				</h1>
-				<div class="newsContent">
-					<?php echo $news['content']; ?>
+				<div class="row ">
+					<div class="col12">
+						<h1 class="newsTitle" id="<?php echo $newsId;?>">
+							<?php echo $news['title']; ?>
+						</h1>
+						<div class="newsContent">
+							<?php echo $news['content']; ?>
+						</div>
+						<div class="newsSignature">
+							<i class="far fa-calendar-alt"></i>
+							<?php echo mb_detect_encoding(strftime('%d %B %Y', $news['publishedOn']), 'UTF-8', true)
+										? strftime('%d %B %Y', $news['publishedOn'])
+										: utf8_encode(strftime('%d %B %Y', $news['publishedOn'])); ?>
+							- <?php echo $this->getData(['user', $news['userId'], 'firstname']) . ' ' . $this->getData(['user', $news['userId'], 'lastname']); ?>
+							<!-- Bloc edition -->
+							<?php if (
+								
+								$this->getUser('password') === $this->getInput('ZWII_USER_PASSWORD')
+								AND
+								(  // Propriétaire
+									(	$this->getUser('group') === self::GROUP_ADMIN )
+								)
+							): ?>
+									<a href ="<?php echo helper::baseUrl() . $this->getUrl(0) . '/edit/' . $newsId . '/' . $_SESSION['csrf'];?>">
+										<?php echo template::ico('pencil');?> Editer
+									</a>
+							<?php endif; ?>
+						</div>
+					</div>
 				</div>
-				<div class="newsSignature">
-					<i class="far fa-calendar-alt"></i>
-					<?php echo mb_detect_encoding(strftime('%d %B %Y', $news['publishedOn']), 'UTF-8', true)
-								? strftime('%d %B %Y', $news['publishedOn'])
-								: utf8_encode(strftime('%d %B %Y', $news['publishedOn'])); ?>
-					- <?php echo $this->getData(['user', $news['userId'], 'firstname']) . ' ' . $this->getData(['user', $news['userId'], 'lastname']); ?>
-				</div>
-				<div class="clearBoth"></div>
-				<hr />
 			<?php endforeach; ?>
 		</div>
 	</div>
