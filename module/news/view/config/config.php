b2969a748edc42c5b3f53fdab511b9e47d98c086
Fred Tempez <ftempez@gmail.com>
2021-03-30 17:58:16 +0200

    Mise en forme de la config

--- module/news/view/config/config.php
+++ module/news/view/config/config.php
@@ -20,46 +20,43 @@
 		</div>
 	</div>
 	<div class="row">
-			<div class="col12">
-				<div class="block">
-					<h4>Paramètres du module</h4>
-					<div class="row">
-						<div class="col6">
-							<?php echo template::checkbox('newsConfigShowFeeds', true, 'Lien du flux RSS', [
-								'checked' => $this->getData(['module', $this->getUrl(0), 'config', 'feeds']),
-								'help' => 'Flux limité aux articles de la première page.'
-							]); ?>
-						</div>
-						<div class="col6">
-							<?php echo template::text('newsConfigFeedslabel', [
-								'label' => 'Texte de l\'étiquette',
-								'value' => $this->getData(['module', $this->getUrl(0), 'config', 'feedsLabel'])
-							]); ?>
-						</div>
+		<div class="col12">
+			<div class="block">
+				<h4>Paramètres du module</h4>
+				<div class="row">
+					<div class="col6">
+						<?php echo template::checkbox('newsConfigShowFeeds', true, 'Lien du flux RSS', [
+							'checked' => $this->getData(['module', $this->getUrl(0), 'config', 'feeds']),
+							'help' => 'Flux limité aux articles de la première page.'
+						]); ?>
+					</div>
+					<div class="col6">
+						<?php echo template::text('newsConfigFeedslabel', [
+							'label' => 'Etiquette RSS',
+							'value' => $this->getData(['module', $this->getUrl(0), 'config', 'feedsLabel'])
+						]); ?>
+					</div>
+				</div>
+				<!-- Block ajouté pour le sélecteur -->	
+				<div class="row">
+					<div class="col6">
+						<?php echo template::select('newsConfigItemsperCol', $module::$Columns, [
+							'label' => 'Pagination',
+							'selected' => $this->getData(['module', $this->getUrl(0),'config', 'itemsperCol']),
+							'help' => 'Nombre de colonnes par page'
+						]); ?>
+					</div>
+					<div class="col6">
+						<?php echo template::select('newsConfigItemsperPage', $module::$ItemsList, [
+							'label' => 'Articles par page',
+							'selected' => $this->getData(['module', $this->getUrl(0),'config', 'itemsperPage']),
+							'help' => 'Nombre de news par page'
+						]); ?>
 					</div>
 				</div>
 			</div>
-	</div>
-<!-- Block ajouté pour le sélecteur -->	
-<div class="block">
-	<h4>Nombre de news par page</h4>
-	<div class="row">
-		<div class="col4">
-			<?php echo template::select('newsConfigItemsperPage', $module::$ItemsList, [
-				'label' => 'News par page',
-				'selected' => $this->getData(['module', $this->getUrl(0),'config', 'itemsperPage']),
-				'help' => 'Nombre de news par page'
-			]); ?>
-		</div>
-		<div class="col4">
-			<?php echo template::select('newsConfigItemsperCol', $module::$Columns, [
-				'label' => 'Nombre de colonne',
-				'selected' => $this->getData(['module', $this->getUrl(0),'config', 'itemsperCol']),
-				'help' => 'Nombre de colonnes par page'
-			]); ?>
 		</div>
 	</div>
-</div>
 	<?php if($module::$news): ?>
 		<?php echo template::table([4, 4, 2, 1, 1], $module::$news, ['Titre', 'Date de publication', 'État', '', '']); ?>
 		<?php echo $module::$pages; ?>
