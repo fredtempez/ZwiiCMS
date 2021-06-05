3f48ab9e1e6959acfd0f76eb9d67e0d3d01bff49
fredtempez <frederic.tempez@outlook.com>
2021-06-05 17:53:07 +0200

    mise à jour de l'url de la feuille de style

--- module/news/news.php
+++ module/news/news.php
@@ -497,6 +497,8 @@ class news extends common {
 			// Déplacer les données du dossier Pages
 			$this->copyDir(self::DATADIRECTORY . 'pages/' . $this->getUrl(0), self::DATADIRECTORY . $this->getUrl(0));
 			$this->removeDir(self::DATADIRECTORY . 'pages/' );
+			$style = $this->getData(['module', $this->getUrl(0), 'theme', 'style']);
+			$this->setData(['module', $this->getUrl(0), 'theme', 'style', str_replace('pages/', '', $style)]);
 			// Mettre à jour la version
 			$this->setData(['module',$this->getUrl(0),'config', 'versionData', '3.3' ]);
 		}
