9d2ae04e887359c0da1e5b8fc817232c4f328b5d
fredtempez <frederic.tempez@outlook.com>
2021-08-30 22:21:32 +0200

    position des boutons

--- core/module/addon/view/upload/upload.php
+++ core/module/addon/view/upload/upload.php
@@ -15,25 +15,22 @@
           'value' => 'Aide'
         ]); ?>
       </div>
+      <div class="col2 ">
+          <?php echo template::button('configModulesStore', [
+              'href' => helper::baseUrl() . 'addon/store',
+              'value' => 'Catalogue en ligne'
+            ]); ?>
+      </div>
+      <div class="col2 offset4">
+            <?php echo template::submit('configModulesSubmit',[
+                'value' => 'Valider',
+                'ico' => 'check'
+            ]); ?>
+      </div>
   </div>
   <!-- Aide à propos de la gestion des modules, view upload -->
   <div class="helpDisplayContent">
     <?php echo file_get_contents( 'core/module/addon/view/upload/upload.help.html') ;?>
-  </div>
-  <div class="row">
-    <div class="col12">
-      <div class="block">
-        <h4>Télécharger un module du catalogue en ligne </h4>
-          <div class="row">
-            <div class="col4 offset4">
-                <?php echo template::button('configModulesStore', [
-                    'href' => helper::baseUrl() . 'addon/store',
-                    'value' => 'Catalogue en ligne'
-                  ]); ?>
-            </div>
-          </div>
-        </div>
-    </div>
   </div>
 	<div class="row">
 		<div class="col12">
@@ -54,12 +51,6 @@
 								'help' => 'Permet de forcer une mise à jour même si la version du module est inférieure ou égale à celle du module installé.',
 							]); ?>
 					</div>
-          <div class="col2">
-            <?php echo template::submit('configModulesSubmit',[
-                'value' => 'Valider',
-                'ico' => 'check'
-            ]); ?>
-      </div>
 				</div>
 			</div>
 		</div>
