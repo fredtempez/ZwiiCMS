d2b4e702fe17ab94c9d47a05ca2728398cdcd49c
Fred Tempez <frederic.tempez@outlook.com>
2021-11-05 14:43:14 +0100

    Bug page parent renommée avec des pages enfants absentes non affichées

--- core/module/page/page.php
+++ core/module/page/page.php
@@ -259,7 +259,7 @@ class page extends common {
 			]);
 		}
 		// Impossible de supprimer une page contenant des enfants
-		elseif($this->getHierarchy($url[0])) {
+		elseif($this->getHierarchy($url[0],null)) {
 			// Valeurs en sortie
 			$this->addOutput([
 				'redirect' => helper::baseUrl() . 'page/edit/' . $url[0],
@@ -327,7 +327,7 @@ class page extends common {
 							$pageId = helper::increment($pageId, self::$coreModuleIds);
 							$pageId = helper::increment($pageId, self::$moduleIds);
 						// Met à jour les enfants
-						foreach($this->getHierarchy($this->getUrl(2)) as $childrenPageId) {
+						foreach($this->getHierarchy($this->getUrl(2),null) as $childrenPageId) {
 							$this->setData(['page', $childrenPageId, 'parentPageId', $pageId]);
 						}
 						// Change l'id de page dans les données des modules
@@ -378,7 +378,7 @@ class page extends common {
 					}
 					// Si la page est une page enfant, actualise les positions des autres enfants du parent, sinon actualise les pages sans parents
 					$lastPosition = 1;
-					$hierarchy = $this->getInput('pageEditParentPageId') ? $this->getHierarchy($this->getInput('pageEditParentPageId')) : array_keys($this->getHierarchy());
+					$hierarchy = $this->getInput('pageEditParentPageId') ? $this->getHierarchy($this->getInput('pageEditParentPageId'),null) : array_keys($this->getHierarchy());
 					$position = $this->getInput('pageEditPosition', helper::FILTER_INT);
 					foreach($hierarchy as $hierarchyPageId) {
 						// Ignore la page en cours de modification
