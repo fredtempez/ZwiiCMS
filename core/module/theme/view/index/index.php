af0f00744a2cb316ac4c6e3003cee5efc8353927
fredtempez <frederic.tempez@outlook.com>
2021-11-10 09:58:11 +0100

    Index du thème

--- core/module/theme/view/index/index.php
+++ core/module/theme/view/index/index.php
@@ -5,7 +5,7 @@
 ): ?>
 	<?php echo template::speech('Cliquez sur une zone afin d\'accéder à ses options de personnalisation. Vous pouvez également afficher les zones cachées à l\'aide du bouton ci-dessous.'); ?>
 	<div class="row">
-		<div class="col3 offset3">
+		<div class="col2 offset3">
 			<?php echo template::button('themeBack', [
 				'class' => 'buttonGrey',
 				'href' => helper::baseUrl(false),
@@ -13,32 +13,16 @@
 				'value' => 'Accueil'
 			]); ?>
 		</div>
-		<div class="col3">
-			<?php echo template::button('themeAdmin', [
-				'ico' => 'brush',
-				'href' => helper::baseUrl() . $this->getUrl(0) . '/admin',
-				'value' => 'Administration'
+		<div class="col2">
+			<?php echo template::button('themeHelp', [
+				'href' => 'https://doc.zwiicms.fr/theme-2',
+				'target' => '_blank',
+				'ico' => 'help',
+				'value' => 'Aide',
+				'class' => 'buttonHelp'
 			]); ?>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col3  offset3">
-			<?php echo template::button('themeManage', [
-				'ico' => 'cogs',
-				'href' => helper::baseUrl() . $this->getUrl(0) . '/manage',
-				'value' => 'Gestion'
-			]); ?>
-		</div>
-		<div class="col3">
-			<?php echo template::button('themeAdvanced', [
-				'ico' => 'code',
-				'href' => helper::baseUrl() . $this->getUrl(0) . '/advanced',
-				'value' => 'Éditeur CSS'
-			]); ?>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col4 offset4">
+		<div class="col2">
 			<?php echo template::button('themeShowAll', [
 				'ico' => 'eye',
 				'value' => 'Zones cachées'
@@ -46,20 +30,33 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col2 offset5">
-			<?php echo template::button('themeBack', [
-				'href' => 'https://doc.zwiicms.fr/theme-2',
-				'target' => '_blank',
-				'ico' => 'help',
-				'value' => 'Aide',
-				'class' => 'buttonHelp'
+		<div class="col2  offset3">
+			<?php echo template::button('themeManage', [
+				'ico' => 'cogs',
+				'href' => helper::baseUrl() . $this->getUrl(0) . '/manage',
+				'value' => 'Gestion'
+			]); ?>
+		</div>
+		<div class="col2">
+		<?php echo template::button('themeAdmin', [
+				'ico' => 'brush',
+				'href' => helper::baseUrl() . $this->getUrl(0) . '/admin',
+				'value' => 'Administration'
+			]); ?>
+
+		</div>
+		<div class="col2">
+			<?php echo template::button('themeAdvanced', [
+				'ico' => 'code',
+				'href' => helper::baseUrl() . $this->getUrl(0) . '/advanced',
+				'value' => 'Éditeur CSS'
 			]); ?>
 		</div>
 	</div>
 <?php else: ?>
 	<?php echo template::speech('Cliquez sur une zone afin d\'accéder à ses options de personnalisation.'); ?>
 	<div class="row">
-		<div class="col3 offset3">
+		<div class="col2 offset4">
 			<?php echo template::button('themeBack', [
 				'class' => 'buttonGrey',
 				'href' => helper::baseUrl(false),
@@ -67,38 +64,37 @@
 				'value' => 'Accueil'
 			]); ?>
 		</div>
-		<div class="col3">
-			<?php echo template::button('themeAdmin', [
-				'ico' => 'brush',
-				'href' => helper::baseUrl() . $this->getUrl(0) . '/admin',
-				'value' => 'Administration'
-			]); ?>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col3 offset3">
-			<?php echo template::button('themeManage', [
-				'href' => helper::baseUrl() . $this->getUrl(0) . '/manage',
-				'ico' => 'cogs',
-				'value' => 'Gestion'
-			]); ?>
-		</div>
-		<div class="col3">
-			<?php echo template::button('themeAdvanced', [
-				'href' => helper::baseUrl() . $this->getUrl(0) . '/advanced',
-				'value' => 'Éditeur CSS',
-				'ico' => 'code'
-			]); ?>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col2 offset5">
-			<?php echo template::button('themeBack', [
+		<div class="col2">
+			<?php echo template::button('themeHelp', [
 				'href' => 'https://doc.zwiicms.fr/theme-2',
 				'target' => '_blank',
 				'ico' => 'help',
 				'value' => 'Aide',
 				'class' => 'buttonHelp'
+			]); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col2  offset3">
+			<?php echo template::button('themeManage', [
+				'ico' => 'cogs',
+				'href' => helper::baseUrl() . $this->getUrl(0) . '/manage',
+				'value' => 'Gestion'
+			]); ?>
+		</div>
+		<div class="col2">
+		<?php echo template::button('themeAdmin', [
+				'ico' => 'brush',
+				'href' => helper::baseUrl() . $this->getUrl(0) . '/admin',
+				'value' => 'Administration'
+			]); ?>
+
+		</div>
+		<div class="col2">
+			<?php echo template::button('themeAdvanced', [
+				'ico' => 'code',
+				'href' => helper::baseUrl() . $this->getUrl(0) . '/advanced',
+				'value' => 'Éditeur CSS'
 			]); ?>
 		</div>
 	</div>
