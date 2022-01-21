86116c6563e8c3d4a179fea75c4fecf60e90a4d6
fredtempez <ftempez@gmail.com>
2022-01-21 08:45:52 +0100

    11.2.04 Check Version

--- core/class/helper.class.php
+++ core/class/helper.class.php
@@ -281,7 +281,7 @@ class helper {
 
 		if($version = helper::getOnlineVersion()) {
 			//return (trim($version) !== common::ZWII_VERSION);
-			return ((version_compare(common::ZWII_VERSION,$version)) === -1);
+			return ((version_compare(common::ZWII_VERSION,$version,'<')) === -1);
 		}
 		else {
 			return false;
