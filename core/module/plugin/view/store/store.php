04d953747aa4ca0fcfba99f9ef117df6e696717c
fredtempez <ftempez@gmail.com>
2022-02-04 08:45:58 +0100

    petites modifs ergonomie

--- core/module/plugin/view/store/store.php
+++ core/module/plugin/view/store/store.php
@@ -6,16 +6,9 @@
 			'value' => template::ico('left')
 		]); ?>
 	</div>
-	<div class="col1 offset10">
-		<?php echo template::button('configStoreUpload', [
-			'href' => helper::baseUrl() . 'plugin/upload',
-			'value' => template::ico('plus'),
-			"help" => 'Importer depuis une archive ZIP'
-		]); ?>
-	</div>
 </div>
 <?php if($module::$storeList): ?>
-	<?php echo template::table([2, 2, 1, 2, 2, 2, 1], $module::$storeList, ['Catégorie', 'Module', 'Version', 'Date', 'Pages', 'Obtenir']); ?>
+	<?php echo template::table([2, 2, 1, 2, 2, 1], $module::$storeList, ['Catégorie', 'Module', 'Version', 'Date', 'Pages', '']); ?>
 <?php else: ?>
 	<?php echo template::speech('Le catalogue est vide.'); ?>
 <?php endif; ?>
