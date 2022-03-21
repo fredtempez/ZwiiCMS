2c981072a6a326e72800b39c2fff5e741199c766
Frédéric Tempez <frederic.tempez@outlook.com>
2022-03-21 19:16:11 +0100

    Gallery affiche directement la galerie seule + bug style absent

--- core/core.php
+++ core/core.php
@@ -2982,8 +2982,7 @@ class core extends common {
 									'style' => file_get_contents($stylePath)
 								]);
 							}
-							if ($output['style'] &&
-								file_exists($output['style'])) {
+							if ($output['style']) {
 									$this->addOutput([
 										'style' => $this->output['style'] . file_get_contents($output['style'])
 									]);
