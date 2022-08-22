fabd73989661246c8b03a847311e92432fdeed41
fredtempez <frederic.tempez@outlook.com>
2022-08-22 21:01:01 +0200

    11.6.00-dev001 configuration du proxy au démarrage

--- core/module/install/view/index/index.php
+++ core/module/install/view/index/index.php
@@ -1,56 +1,94 @@
 <p>Renseignez les champs ci-dessous pour finaliser l'installation.</p>
 <?php echo template::formOpen('installForm'); ?>
-	<?php echo template::text('installId', [
-		'autocomplete' => 'off',
-		'label' => 'Identifiant'
-	]); ?>
-	<div class="row">
-		<div class="col6">
-			<?php echo template::password('installPassword', [
-				'autocomplete' => 'off',
-				'label' => 'Mot de passe'
-			]); ?>
-		</div>
-		<div class="col6">
-			<?php echo template::password('installConfirmPassword', [
-				'autocomplete' => 'off',
-				'label' => 'Confirmation'
-			]); ?>
-		</div>
-	</div>
-	<?php echo template::mail('installMail', [
-		'autocomplete' => 'off',
-		'label' => 'Adresse mail'
-	]); ?>
-	<div class="row">
-		<div class="col6">
-			<?php echo template::text('installFirstname', [
-				'autocomplete' => 'off',
-				'label' => 'Prénom'
-			]); ?>
-		</div>
-		<div class="col6">
-			<?php echo template::text('installLastname', [
-				'autocomplete' => 'off',
-				'label' => 'Nom'
-			]); ?>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col12">
-		<?php echo template::select('installTheme', $module::$themes, [
-				'label' => 'Thème'
-			]); ?>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col12">
-			<?php echo template::checkbox('installDefaultData',true , 'Ne pas charger l\'exemple de site (utilisateurs avancés)', [
-				'checked' => false
-			]);
-			?>
-		</div>
-	</div>
+	<ul class="accordion" data-speed="150">
+		<li class="accordion-item active">
+			<h3 class="accordion-title">Compte administrateur</h3>
+			<div class="accordion-content">
+				<div class="row">
+					<div class="col12">
+						<?php echo template::text('installId', [
+							'autocomplete' => 'off',
+							'label' => 'Identifiant'
+						]); ?>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col6">
+						<?php echo template::password('installPassword', [
+							'autocomplete' => 'off',
+							'label' => 'Mot de passe'
+						]); ?>
+					</div>
+					<div class="col6">
+						<?php echo template::password('installConfirmPassword', [
+							'autocomplete' => 'off',
+							'label' => 'Confirmation'
+						]); ?>
+					</div>
+				</div>
+				<?php echo template::mail('installMail', [
+					'autocomplete' => 'off',
+					'label' => 'Adresse mail'
+				]); ?>
+				<div class="row">
+					<div class="col6">
+						<?php echo template::text('installFirstname', [
+							'autocomplete' => 'off',
+							'label' => 'Prénom'
+						]); ?>
+					</div>
+					<div class="col6">
+						<?php echo template::text('installLastname', [
+							'autocomplete' => 'off',
+							'label' => 'Nom'
+						]); ?>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="accordion-item">
+			<h3 class="accordion-title">Options avancées</h3>
+			<div class="accordion-content">
+				<div class="row">
+					<div class="col12">
+						<?php echo template::checkbox('installDefaultData',true , 'Ne pas charger l\'exemple de site (utilisateurs avancés)', [
+							'checked' => false
+						]);
+						?>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col3">
+						<?php echo template::select('installProxyType', $module::$proxyType, [
+							'label' => 'Type de proxy',
+							'selected' => $this->getData(['config', 'proxyType'])
+							]); ?>
+						</div>
+					<div  class="col6">
+						<?php echo template::text('installProxyUrl', [
+							'label' => 'Adresse du proxy',
+							'placeholder' => 'cache.proxy.fr',
+							'value' => $this->getData(['config', 'proxyUrl'])
+						]); ?>
+					</div>
+					<div  class="col3">
+						<?php echo template::text('installProxyPort', [
+							'label' => 'Port du proxy',
+							'placeholder' => '6060',
+							'value' => $this->getData(['config', 'proxyPort'])
+						]); ?>
+					</div>
+					<div class="row">
+						<div class="col12">
+						<?php echo template::select('installTheme', $module::$themes, [
+								'label' => 'Thème'
+							]); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+	</ul>
 	<div class="row">
 		<div class="col3 offset9">
 			<?php echo template::submit('installSubmit', [
