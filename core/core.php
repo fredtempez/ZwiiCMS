9e74d13e98338620a0824b5d39d3862dfc592c17
Fred Tempez <frederic.tempez@outlook.com>
2022-09-21 16:05:22 +0200

    Enlever des langues

--- core/core.php
+++ core/core.php
@@ -154,41 +154,19 @@ class common {
 	// Langue de l'interface sélectionnée
 	public static $i18nUI = 'fr_FR';
 	public static $languagesUI = [
-		'az_AZ' => 'Azərbaycan dili',
-		'bg_BG' => 'български език',
 		'ca' => 'Català, valencià',
-		'cs' => 'čeština, český jazyk',
 		'da' => 'Dansk',
 		'de' => 'Deutsch',
-		'el_GR' => 'ελληνικά',
 		'en_EN' => 'English',
 		'es' => 'Español',
-		'fa' => 'فارسی',
 		'fr_FR' => 'Français',
-		'he_IL' => 'Hebrew (Israel)',
-		'hr' => 'Hrvatski jezik',
-		'hu_HU' => 'Magyar',
-		'id' => 'Bahasa Indonesia',
 		'it' => 'Italiano',
-		'ja' => '日本',
-		'lt' => 'Lietuvių kalba',
-		'mn_MN' => 'монгол',
-		'nb_NO' => 'Norsk bokmål',
-		'nn_NO' => 'Norsk nynorsk',
 		'nl' => 'Nederlands, Vlaams',
 		'pl' => 'Język polski, polszczyzna',
-		'pt_BR' => 'Português(Brazil)',
 		'pt_PT' => 'Português',
 		'ro' => 'Română',
-		'ru' => 'Pусский язык',
-		'sk' => 'Slovenčina',
-		'sl' => 'Slovenski jezik',
 		'sv_SE' => 'Svenska',
-		'th_TH' => 'ไทย',
-		'tr_TR' => 'Türkçe',
-		'uk_UA' => 'Yкраїнська мова',
-		'vi' => 'Tiếng Việt',
-		'zh_CN' => '中文 (Zhōngwén), 汉语, 漢語',
+		'tr_TR' => 'Türkçe'
 	
 		// source: http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 	];
@@ -196,12 +174,13 @@ class common {
 	// Langues de contenu
 	public static $languagesContent = [
 		'fr'	=> 'Français (fr)',
-		'de' 	=> 'Allemand (de)',
-		'en'	=> 'Anglais (en)',
-		'es'	=> 'Espagnol (es)',
-		'it'	=> 'Italien (it)',
-		'nl' 	=> 'Néerlandais (nl)',
-		'pt'	=> 'Portugais (pt)',
+		'de' 	=> 'Deutsch (de)',
+		'en'	=> 'English (en)',
+		'es'	=> 'Español (es)',
+		'it'	=> 'Italiano (it)',
+		'nl' 	=> 'Nederlands, Vlaams (nl)',
+		'pt'	=> 'Português (pt)',
+		'tr' 	=> 'Türkçe'
 	];
 	// Sélection de langue de l'interface
 	public static $i18nContent = 'fr';
