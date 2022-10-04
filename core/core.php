d1d76aaf81dbbc15ef3997cffaca4e63712bd81c
Fred Tempez <frederic.tempez@outlook.com>
2022-10-04 10:21:39 +0200

    Paramètre null passé

--- core/core.php
+++ core/core.php
@@ -1324,7 +1324,8 @@ class common {
 	public function showSection() {
 		echo '<section>';
 		// Récupérer la config de la page courante
-		$blocks = explode('-',$this->getData(['page',$this->getUrl(0),'block']));
+		$blocks = is_null($this->getData(['page',$this->getUrl(0),'block'])) ? '12' : $this->getData(['page',$this->getUrl(0),'block']);
+		$blocks = explode('-', $blocks);
 		// Initialiser
 		$blockleft=$blockright="";
 		switch (sizeof($blocks)) {
