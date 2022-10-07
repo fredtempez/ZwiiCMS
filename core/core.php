98ab381b0ea6d9defdc4ff448d25e83c58414f7e
Frédéric Tempez <frederic.tempez@outlook.com>
2022-10-07 17:40:53 +0200

    Cookie consent double url si page accueil

--- core/core.php
+++ core/core.php
@@ -1329,7 +1329,13 @@ class common
 			$item .= '<h3>' . $this->getData(['locale', 'cookies', 'titleLabel']) . '</h3>';
 			$item .= '<p>' . $this->getData(['locale', 'cookies', 'mainLabel']) . '</p>';
 			// Formulaire de réponse
-			$item .= '<form method="POST" action="' . helper::baseUrl() . $this->getUrl() . '" id="cookieForm">';
+			if (
+				$this->getData(['locale', 'homePageId']) === $this->getUrl(0)
+			) {
+				$item .= '<form method="POST" action="' . helper::baseUrl(false) . '" id="cookieForm">';
+			} else {
+				$item .= '<form method="POST" action="' . helper::baseUrl(true) . $this->getUrl(). '" id="cookieForm">';
+			}
 			$analytics = $this->getData(['config', 'seo', 'analyticsId']);
 			$stateCookieGA = $this->getInput('ZWII_COOKIE_GA_CONSENT') ===  'true' ? 'checked="checked"' : '';
 			if ($analytics !== null and $analytics !== '') {
