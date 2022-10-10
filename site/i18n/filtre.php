77a22feb4e2ba9fa0252c663f9e4085a9076522e
Frédéric Tempez <frederic.tempez@outlook.com>
2022-10-10 16:44:20 +0200

    chaines des modules incomplètes

---
+++ site/i18n/filtre.php
@@ -0,0 +1,16 @@
+<?php
+    $target = 'search';
+
+
+    $data = json_decode(file_get_contents('fr_FR.json'), true);
+    $dataTarget = json_decode(file_get_contents($target . '.json'), true);
+
+    foreach($dataTarget as $key => $value)  {
+        if (array_key_exists($key, $data)) {
+            unset($dataTarget[$key]);
+            echo $key;
+            echo "<p>";
+        }
+    }
+
+    file_put_contents ($target . '.json', json_encode($dataTarget, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT), LOCK_EX);
