d0818a52dff48a8e212f2de8bf169e74d6f9807a
Frédéric Tempez <frederic.tempez@outlook.com>
2022-10-15 09:02:27 +0200

    dev0014

--- core/core.php
+++ core/core.php
@@ -49,7 +49,7 @@ class common
 	// Numéro de version
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/update/raw/branch/master/';
 
-	const ZWII_VERSION = '12.0.00-dev013';
+	const ZWII_VERSION = '12.0.00-dev014';
 	const ZWII_UPDATE_CHANNEL = "test";
 
 	public static $actions = [];
