1de61779295d079cfc049dd499450cd9d622359b
Fred Tempez <frederic.tempez@outlook.com>
2022-11-15 08:13:57 +0100

    forgot password

--- core/module/user/view/forgot/forgot.php
+++ core/module/user/view/forgot/forgot.php
@@ -3,13 +3,13 @@
 	'label' => 'Identifiant'
 ]); ?>
 <div class="row">
-	<div class="col1 offset9">
+	<div class="col2">
 		<?php echo template::button('userForgotBack', [
 			'href' => helper::baseUrl() . 'user/login/' . $this->getUrl(2),
 			'value' => template::ico('left')
 		]); ?>
 	</div>
-	<div class="col3">
+	<div class="col3 offset7">
 		<?php echo template::submit('userForgotSubmit', [
 			'value' => 'Valider'
 		]); ?>
