c7532807719587dd7ecd296b24649db57848a0ef
Frédéric Tempez <frederic.tempez@outlook.com>
2022-11-22 18:02:10 +0100

    var_dump dans la notification

--- core/core.php
+++ core/core.php
@@ -2092,7 +2092,6 @@ class common
 		if (common::$inputNotices) {
 			$notification = 'Impossible de soumettre le formulaire, car il contient des erreurs';
 			$notificationClass = 'notificationError';
-			var_dump(common::$inputNotices);
 		}
 		if (common::$coreNotices) {
 			$notification = 'Données absentes, restauration de <p> | ';
