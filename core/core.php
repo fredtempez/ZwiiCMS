9c924a75e28a6a1f451c2927a6b7a53a3043caf3
Fred Tempez <frederic.tempez@outlook.com>
2022-12-13 11:19:37 +0100

    beta05 accessibilité

--- core/core.php
+++ core/core.php
@@ -49,7 +49,7 @@ class common
 	// Numéro de version
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/update/raw/branch/master/';
 
-	const ZWII_VERSION = '12.0.00-beta04';
+	const ZWII_VERSION = '12.0.00-beta05';
 	const ZWII_UPDATE_CHANNEL = "test";
 
 	public static $actions = [];
