247087b48180677812474bdf969f06c019016ec3
Fred Tempez <frederic.tempez@outlook.com>
2023-02-07 08:36:19 +0100

    12204 sort const

--- core/core.php
+++ core/core.php
@@ -46,21 +46,21 @@ class common
 	// Contrôle d'édition temps maxi en secondes avant déconnexion 30 minutes
 	const ACCESS_TIMER = 1800;
 
+	// Numéro de version et branche pour l'auto-update
+	const ZWII_VERSION = '12.2.04';
+
+	const ZWII_DATAVERSION = 12000;
+
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/update/raw/branch/master/';
 	const ZWII_UPDATE_CHANNEL = "v12";
 	
 	// Constantes de test
-	//const ZWII_UPDATE_CHANNEL = "test";
 	//const ZWII_UPDATE_URL = 'http://localhost/update/';
+	//const ZWII_UPDATE_CHANNEL = "test";
 
 	// URL langues de l'UI en ligne
 	const ZWII_UI_URL = 'https://forge.chapril.org/ZwiiCMS-Team/zwiicms-translations/raw/branch/master/';
-
-	// Numéro de version et branche pour l'auto-update
-	const ZWII_VERSION = '12.2.04';
-
-	const ZWII_DATAVERSION = 12000;
 
 
 	public static $actions = [];
