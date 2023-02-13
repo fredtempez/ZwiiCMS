5bc5e529368bcca237d9f3f997ec1c4a9d2a82b5
Fred Tempez <frederic.tempez@outlook.com>
2023-02-13 14:39:09 +0100

    12300 module news traductions

--- module/news/view/add/add.php
+++ module/news/view/add/add.php
@@ -26,7 +26,7 @@
 	<div class="row">
 		<div class="col12">
 			<div class="block">
-				<h4>Informations générales</h4>
+				<h4><?php echo helper::translate('Informations générales');?></h4>
 				<?php echo template::text('newsAddTitle', [
 					'label' => 'Titre'
 				]); ?>
@@ -39,7 +39,7 @@
 	<div class="row">
 		<div class="col12">
 			<div class="block">
-				<h4>Options de publication</h4>
+				<h4><?php echo helper::translate('Options de publication');?></h4>
 				<div class="row">
 					<div class="col4">
 						<?php echo template::select('newsAddUserId', $module::$users, [
