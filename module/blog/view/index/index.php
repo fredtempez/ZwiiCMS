c85f208837092a3410c1d17302d276f7518d974b
Fred Tempez <frederic.tempez@outlook.com>
2023-03-23 07:56:45 +0100

    Lire la suite WIP

--- module/blog/view/index/index.php
+++ module/blog/view/index/index.php
@@ -147,7 +147,8 @@
 							<p class="blogContent">
 								<?php $lenght = $this->getData(['module', $this->getUrl(0), 'config', 'articlesLenght']) !== 0 ? $this->getData(['module', $this->getUrl(0), 'config', 'articlesLenght']) : 500 ?>
 								<?php echo helper::subword(strip_tags($article['content'], '<br><p>'), 0, $lenght); ?>...
-								<a href="<?php echo helper::baseUrl() . $this->getUrl(0) . '/' . $articleId; ?>">Lire la suite</a>
+								<a href="<?php echo helper::baseUrl() . $this->getUrl(0) . '/' . $articleId; ?>"><button class="read-more-btn">Lire la suite</button></a>
+								
 							</p>
 						</div>
 					</div>
