28837e7088d8abca97b57e3755d2e13cebb2db13
fredtempez <frederic.tempez@outlook.com>
2023-06-19 19:48:45 +0200

    12312

--- core/core.php
+++ core/core.php
@@ -53,7 +53,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version et branche pour l'auto-update
-	const ZWII_VERSION = '12.3.11';
+	const ZWII_VERSION = '12.3.12';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/update/raw/branch/master/';
