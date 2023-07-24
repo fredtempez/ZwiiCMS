3e5332964600c3ccba597e1f773c75eea3275e2d
Fred Tempez <frederic.tempez@outlook.com>
2023-07-24 18:36:48 +0200

    build 4

--- core/core.php
+++ core/core.php
@@ -51,7 +51,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '13.0.00.3';
+	const ZWII_VERSION = '13.0.00.4';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/update/raw/branch/master/';
