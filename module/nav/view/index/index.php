63ef8f8f347c66e3581d7adb8e6147e3a9e9729f
fredtempez <frederic.tempez@outlook.com>
2023-08-29 19:52:11 +0200

    Déplace navButton

--- module/nav/view/index/index.php
+++ module/nav/view/index/index.php
@@ -1,18 +1,20 @@
-<div class="row navButton">
-    <div class="col1 textAlignCenter">
-        <?php if ($module::$previousPage !== null) {
-            echo template::button('navPreviousButton', [
-                'href' => helper::baseUrl() . $module::$previousPage,
-                'value' => template::ico($module::$leftButton)
-            ]);
-        } ?>
-    </div>
-    <div class="col1 offset10 textAlignCenter">
-        <?php if ($module::$nextPage !== null) {
-            echo template::button('nabNextButton', [
-                'href' => helper::baseUrl() . $module::$nextPage,
-                'value' => template::ico($module::$rightButton)
-            ]);
-        } ?>
+<div class="navButton">
+    <div class="row">
+        <div class="col1 textAlignCenter">
+            <?php if ($module::$previousPage !== null) {
+                echo template::button('navPreviousButton', [
+                    'href' => helper::baseUrl() . $module::$previousPage,
+                    'value' => template::ico($module::$leftButton)
+                ]);
+            } ?>
+        </div>
+        <div class="col1 offset10 textAlignCenter">
+            <?php if ($module::$nextPage !== null) {
+                echo template::button('nabNextButton', [
+                    'href' => helper::baseUrl() . $module::$nextPage,
+                    'value' => template::ico($module::$rightButton)
+                ]);
+            } ?>
+        </div>
     </div>
 </div>
