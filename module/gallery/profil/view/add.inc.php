2e76d9d30597d45932ee02d5bbc60a0124588264
F TEMPEZ <frederic.tempez@outlook.com>
2023-11-07 10:10:20 +0100

    Termes de profils gallery

--- module/gallery/profil/view/add.inc.php
+++ module/gallery/profil/view/add.inc.php
@@ -6,19 +6,19 @@
             </h4>
             <div class="row">
                 <div class="col3">
-                    <?php echo template::checkbox('profilAddGalleryAdd', true, 'Ajouter'); ?>
+                    <?php echo template::checkbox('profilAddGalleryAdd', true, 'Ajouter une galerie'); ?>
                 </div>
                 <div class="col3">
-                    <?php echo template::checkbox('profilAddGalleryEdit', true, 'Éditer'); ?>
+                    <?php echo template::checkbox('profilAddGalleryEdit', true, 'Éditer une galerie'); ?>
                 </div>
                 <div class="col2">
-                    <?php echo template::checkbox('profilAddGalleryDelete', true, 'Effacer'); ?>
+                    <?php echo template::checkbox('profilAddGalleryDelete', true, 'Effacer une galerie'); ?>
                 </div>
                 <div class="col2">
-                    <?php echo template::checkbox('profilAddGalleryOption', true, 'Options'); ?>
+                    <?php echo template::checkbox('profilAddGalleryOption', true, 'Options des galeries'); ?>
                 </div>
                 <div class="col2">
-                    <?php echo template::checkbox('profilAddGalleryTheme', true, 'Thème'); ?>
+                    <?php echo template::checkbox('profilAddGalleryTheme', true, 'Thème des galeries'); ?>
                 </div>
             </div>
         </div>
--- module/gallery/profil/view/edit.inc.php
+++ module/gallery/profil/view/edit.inc.php
@@ -6,27 +6,27 @@
             </h4>
             <div class="row">
                 <div class="col3">
-                    <?php echo template::checkbox('profilEditGalleryAdd', true, 'Ajouter', [
+                    <?php echo template::checkbox('profilEditGalleryAdd', true, 'Ajouter une galerie', [
                         'checked' => $this->getData(['profil', $this->getUrl(2), $this->getUrl(3), 'gallery', 'add'])
                     ]); ?>
                 </div>
                 <div class="col2">
-                    <?php echo template::checkbox('profilEditGalleryEdit', true, 'Éditer', [
+                    <?php echo template::checkbox('profilEditGalleryEdit', true, 'Éditer une galerie', [
                         'checked' => $this->getData(['profil', $this->getUrl(2), $this->getUrl(3), 'gallery', 'edit'])
                     ]); ?>
                 </div>
                 <div class="col2">
-                    <?php echo template::checkbox('profilEditGalleryDelete', true, 'Effacer', [
+                    <?php echo template::checkbox('profilEditGalleryDelete', true, 'Effacer une galerie', [
                         'checked' => $this->getData(['profil', $this->getUrl(2), $this->getUrl(3), 'gallery', 'delete'])
                     ]); ?>
                 </div>
                 <div class="col2">
-                    <?php echo template::checkbox('profilEditGalleryOption', true, 'Options', [
+                    <?php echo template::checkbox('profilEditGalleryOption', true, 'Options des galeries', [
                         'checked' => $this->getData(['profil', $this->getUrl(2), $this->getUrl(3), 'gallery', 'option'])
                     ]); ?>
                 </div>
                 <div class="col2">
-                    <?php echo template::checkbox('profilEditGalleryTheme', true, 'Thème', [
+                    <?php echo template::checkbox('profilEditGalleryTheme', true, 'Thème des galeries', [
                         'checked' => $this->getData(['profil', $this->getUrl(2), $this->getUrl(3), 'gallery', 'theme'])
                     ]); ?>
                 </div>
