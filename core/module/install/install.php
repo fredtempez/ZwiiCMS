f9f55be851accc6ba233cbef17290030b8ad89df
fredtempez <frederic.tempez@outlook.com>
2023-11-15 14:10:02 +0100

    Profil de l'admin manquant à l'installation

--- core/module/install/install.php
+++ core/module/install/install.php
@@ -150,6 +150,7 @@ class install extends common
 						'firstname' => $userFirstname,
 						'forgot' => 0,
 						'group' => self::GROUP_ADMIN,
+						'profil' => 0,
 						'lastname' => $userLastname,
 						'pseudo' => 'Admin',
 						'signature' => 1,
