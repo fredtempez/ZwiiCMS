03d1dacb8857d4390c828c65ff1bcb76a060161a
Fred Tempez <frederic.tempez@outlook.com>
2024-04-05 18:21:51 +0200

    détruit le fichier de verouillage créé par secureFilePutContent

--- core/core.php
+++ core/core.php
@@ -658,6 +658,9 @@ class common
                 }
             } else {
                 fclose($lockHandle);
+				if (file_exists($lockHandle)) {
+					unlink($lockHandle);
+				}
             }
             $attempts++;
             error_log('Erreur d\'écriture (tentative ' . $attempts . ') : impossible de sauvegarder les données dans ' . $filename);
