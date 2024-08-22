89be29046534cd7a9657231db06e76b1cebbbbd0
Fred Tempez <frederic.tempez@outlook.com>
2024-08-22 05:31:27 +0200

    13.3.06 couleurs des balises h1, h3 et A dans le slider

--- core/core.php
+++ core/core.php
@@ -51,7 +51,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '13.3.05';
+	const ZWII_VERSION = '13.3.06';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/cms-update/raw/branch/master/';
