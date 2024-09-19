7a4b1589afeead3f197e70eb9cebc9a7da813ec5
Frédéric Tempez <frederic.tempez@outlook.com>
2024-09-19 10:52:11 +0200

    13.4.00.test4 inverse la position des icones users et config

--- core/core.php
+++ core/core.php
@@ -51,7 +51,7 @@ class common
 	const ACCESS_TIMER = 1800;
 
 	// Numéro de version
-	const ZWII_VERSION = '13.4.00.test3';
+	const ZWII_VERSION = '13.4.00.test4';
 
 	// URL autoupdate
 	const ZWII_UPDATE_URL = 'https://forge.chapril.org/ZwiiCMS-Team/cms-update/raw/branch/master/';
