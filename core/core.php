041c23ed2a5e50fb87cb06e42aa1374523973925
Frédéric Tempez <frederic.tempez@outlook.com>
2024-12-23 15:44:35 +0100

    Branche 13

--- core/core.php
+++ core/core.php
@@ -62,7 +62,7 @@ class common
 	 * La branche v13 est maintenue afin de télécharger un correctif permettant d'installer
 	 * les version supérieures. 
 	*/ 
-	const ZWII_UPDATE_CHANNEL = 'v134';
+	const ZWII_UPDATE_CHANNEL = 'v13';
 
 	// Valeurs possibles multiple de 10, 10 autorise 9 profils, 100 autorise 99 profils
 	const MAX_PROFILS = 10;
