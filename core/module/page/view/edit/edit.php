2bb11c8754e01808d3977d6aed194e1207a33b07
Frédéric Tempez <frederic.tempez@outlook.com>
2025-01-28 18:22:20 +0100

    Bug édition de page

--- core/module/page/view/edit/edit.php
+++ core/module/page/view/edit/edit.php
@@ -119,7 +119,7 @@ echo '></div>';
 	</div>
 	<div class="row">
 		<div class="col12">
-			<?php echo template::textarea('pageEditContent', [
+			<?php echo template::textarea('editorWysiwyg', [
 				'class' => 'editorWysiwyg',
 				'value' => $this->getPage($this->getUrl(2), self::$siteContent)
 			]); ?>
